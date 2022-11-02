b21154cbacd44d6ea3b05becdfa5925ea96b6129
Chris Johnson <chris.johnson@cosocloud.com>
2022-11-01 21:04:47 -0400

    Small refactor

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # Importing requried modules
 import os
+import time
 import maskpass
 import numpy
 from termcolor import cprint
 from cryptography.fernet import Fernet
 
 # Different OS use different commands to clear the screen, linux:clear win:cls
-CLEAR_SCREEN = 'cls'
+CLEAR_SCREEN = 'clear'
 
 # Defining The Correct Password and The Password Attempt amount
 correct_password = 'password1'  # Need to remove.  Not used anymore.
-PASSWORD_ATTEMPT = 11
+PASSWORD_ATTEMPT = 3
 
 # Function to check if the Users password is correct
 
@@ -31,41 +32,6 @@ def check_user(passwd):
             return True
     # For security, we return False if the bool expression never matches True.
     return False
-
-
-# Clearing the Screen
-os.system(CLEAR_SCREEN)
-
-# Defining password loop
-while True:
-
-    # Checking if the user trys to bypass the password with a keyboard interrupt
-    try:
-
-        # Getting the users password input with a security mask
-        cprint('[*]Password: ', 'blue', end="")
-        password_input = maskpass.askpass(prompt="", mask="*")
-
-        # Checking if password is the correct password, if so we break from loop
-        if check_user(password_input):
-            break
-
-        # Else, we subtract one from their attempts , and print it to the user
-        PASSWORD_ATTEMPT -= 1
-        cprint(
-            f'[-] Wrong password, you have {PASSWORD_ATTEMPT} remaining', 'red')
-
-        # If the user has run out of password attempts then we clear the screen and quit the program
-        if PASSWORD_ATTEMPT == 0:
-            os.system(CLEAR_SCREEN)
-            cprint('[-] You have run out of password attempt, goodbye', 'red')
-            exit()
-
-    except KeyboardInterrupt:
-
-        # A taunting message is displayed if the user tries to use KeyboardInterrupt, and the loop continues
-        cprint("\n[!] Nice try, it won't be that easy", 'red')
-        continue
 
 
 def firgureMath(mathEquation):
@@ -99,8 +65,45 @@ def mathResults(operator: str, mathEquation: str):
             return accumulate
 
 
-math_equation_problem = ['1 + 2', '5 * 2', '3 - 2 - 1', '25/5/ 5', '23/5']
-for math_equation in math_equation_problem:
-    cprint(f'[*]Problem: {math_equation}', 'blue')
-    cprint(
-        f'[+]Results:{mathResults(firgureMath(math_equation), math_equation)}\n\n', 'green')
+# Clearing the Screen
+os.system(CLEAR_SCREEN)
+
+# Main app begins here
+while True:
+
+    # Checking if the user trys to bypass the password with a keyboard interrupt
+    try:
+
+        # Getting the users password input with a security mask
+        cprint('[*]Password: ', 'blue', end="")
+        password_input = maskpass.askpass(prompt="", mask="*")
+
+        # Checking if password is the correct password, if so we break from loop
+        if check_user(password_input):
+            math_equation_problem = ['1 + 2', '5 * 2',
+                                     '3 - 2 - 1', '25/5/ 5', '23/5']
+            for math_equation in math_equation_problem:
+                cprint(f'[*]Problem: {math_equation}', 'blue')
+                cprint(
+                    f'[+]Results:{mathResults(firgureMath(math_equation), math_equation)}\n\n', 'green')
+
+        # Else, we subtract one from their attempts , and print it to the user
+        else:
+            PASSWORD_ATTEMPT -= 1
+            cprint(
+                f'[-] Wrong password, you have {PASSWORD_ATTEMPT} remaining', 'red')
+
+        # If the user has run out of password attempts then we clear the screen and quit the program
+        if PASSWORD_ATTEMPT == 0:
+            os.system(CLEAR_SCREEN)
+            cprint('[-] You have run out of password attempts.\n[-] Locked for 10 seconds.', 'red')
+            time.sleep(10)
+            cprint('[-] Resetting password atempts post lockout', 'yellow')
+            PASSWORD_ATTEMPT = 3
+            # exit()
+
+    except KeyboardInterrupt:
+
+        # A taunting message is displayed if the user tries to use KeyboardInterrupt, and the loop continues
+        cprint("\n[!] Nice try, it won't be that easy", 'red')
+        continue
